99938565a59c9602b10d4d3a010a15050d8307db
Francisco Abarzúa <git.id@frabarz.cl>
2019-07-02 14:39:17 -0400

    updates url for regex verification

--- packages/create-tesseract-ui/src/validate.js
+++ packages/create-tesseract-ui/src/validate.js
@@ -5,8 +5,6 @@ module.exports = {
     return /[a-z0-9-]/i.test(string);
   },
   url(string) {
-    return /(https?):\/\/[\w@:%.\+~#=]{2,253}\.[a-z]{2,10}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(
-      string
-    );
+    return /https?:\/\/[\w-@:%.~=]{2,256}\b[\w-@:%\+.~/=]*/i.test(string);
   }
 };
