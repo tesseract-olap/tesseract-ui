c3a7b0aa5bf1dc501578d3c2e25a8580eb5a147e
Francisco Abarzúa <git.id@frabarz.cl>
2019-07-13 02:20:40 -0400

    fixes clipboard behavior on DataTable

--- packages/tesseract-explorer/src/components/TableTabPanel.jsx
+++ packages/tesseract-explorer/src/components/TableTabPanel.jsx
@@ -3,7 +3,15 @@ import {Cell, Column, Table} from "@blueprintjs/table";
 import cn from "classnames";
 import React from "react";
 
-function TableTabPanel(props) {
+/**
+ * @typedef OwnProps
+ * @property {(row: number, col: number) => any} cellClipboardData
+ * @property {string} className
+ * @property {any[]} data
+ */
+
+/** @type {React.FC<OwnProps>} */
+const TableTabPanel = function(props) {
   const {data} = props;
 
   if (data.length === 0) {
@@ -16,25 +24,37 @@ function TableTabPanel(props) {
     );
   }
 
-  const columns = Object.keys(data[0]).map(key => {
-    const cellRenderer = rowIndex => <Cell>{data[rowIndex][key]}</Cell>;
-    return <Column key={key} name={key} cellRenderer={cellRenderer} />;
+  const columnKeys = Object.keys(data[0]);
+  const columns = columnKeys.map((key, columnIndex) => {
+    const cellRenderer = rowIndex => (
+      <Cell columnIndex={columnIndex} rowIndex={rowIndex}>
+        {data[rowIndex][key]}
+      </Cell>
+    );
+    return <Column key={key} id={key} name={key} cellRenderer={cellRenderer} />;
   });
+
+  const cellClipboardData = props.cellClipboardData.bind(null, data, columnKeys);
 
   return (
     <Table
       className={cn("data-table", props.className)}
+      enableColumnResizing={false}
+      // enableGhostCells={true}
+      // enableMultipleSelection={false}
+      enableRowResizing={false}
+      getCellClipboardData={cellClipboardData}
       numRows={data.length}
-      allowMultipleSelection={false}
-      // columnWidths={columnWidths}
-      fillBodyWithGhostCells={true}
-      isColumnResizable={false}
-      isRowResizable={false}
       rowHeights={data.map(() => 22)}
     >
       {columns}
     </Table>
   );
-}
+};
+
+TableTabPanel.defaultProps = {
+  cellClipboardData: (data, keys, rowIndex, columnIndex) =>
+    data[rowIndex][keys[columnIndex]]
+};
 
 export default TableTabPanel;
