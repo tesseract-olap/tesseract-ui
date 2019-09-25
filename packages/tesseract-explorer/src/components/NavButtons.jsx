8fc0b16965a25140640ca117e01aade22eae1702
Francisco Abarzúa <git.id@frabarz.cl>
2019-09-25 15:36:42 -0300

    adds permalink copy button to navbar

--- packages/tesseract-explorer/src/components/NavButtons.jsx
+++ packages/tesseract-explorer/src/components/NavButtons.jsx
@@ -1,15 +1,19 @@
 import {Button, ButtonGroup} from "@blueprintjs/core";
 import React from "react";
 import {connect} from "react-redux";
+import copy from "clipboard-copy";
 import {toggleDarkTheme, toggleDebugDrawer, toggleStarredDrawer} from "../actions/ui";
+import { selectPermalink } from "../selectors/permalink";
 
 /**
  * @typedef StateProps
  * @property {boolean} darkTheme
+ * @property {string} permalink
  */
 
 /**
  * @typedef DispatchProps
+ * @property {() => any} copyPermalink
  * @property {() => any} toggleDebugDrawerHandler
  * @property {() => any} toggleStarredDrawerHandler
  * @property {() => any} toggleThemeHandler
@@ -23,16 +27,25 @@ const NavbarButtons = function(props) {
         className="button-toggle-theme"
         icon={props.darkTheme ? "flash" : "moon"}
         onClick={props.toggleThemeHandler}
+        title={props.darkTheme ? "Switch to light theme" : "Switch to dark theme"}
       />
       <Button
         className="button-toggle-starred"
         icon="star"
         onClick={props.toggleStarredDrawerHandler}
+        title="Starred items drawer"
       />
       <Button
         className="button-toggle-debug"
         icon="code-block"
         onClick={props.toggleDebugDrawerHandler}
+        title="Debug drawer"
+      />
+      <Button
+        className="button-copy-permalink"
+        icon="link"
+        onClick={props.copyPermalink.bind(null, props.permalink)}
+        title="Copy permalink"
       />
     </ButtonGroup>
   );
@@ -41,13 +54,19 @@ const NavbarButtons = function(props) {
 /** @type {import("react-redux").MapStateToProps<StateProps, {}, import("../reducers").ExplorerState>} */
 function mapStateToProps(state) {
   return {
-    darkTheme: state.explorerUi.darkTheme
+    darkTheme: state.explorerUi.darkTheme,
+    permalink: selectPermalink(state)
   };
 }
 
 /** @type {import("react-redux").MapDispatchToPropsFunction<DispatchProps, {}>} */
 function mapDispatchToProps(dispatch) {
   return {
+    copyPermalink(permalink, evt) {
+      evt.stopPropagation();
+      const {origin, pathname} = window.location;
+      return copy(`${origin}${pathname}?${decodeURI(`${permalink}`)}`);
+    },
     toggleDebugDrawerHandler() {
       return dispatch(toggleDebugDrawer());
     },
