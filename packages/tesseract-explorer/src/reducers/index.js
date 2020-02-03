63ee8d037b5f23607c9d338d84962666265c4c53
Francisco Abarzúa <git.id@frabarz.cl>
2020-02-03 18:30:48 -0300

    solves issue in main reducer on initial state

--- packages/tesseract-explorer/src/reducers/index.js
+++ packages/tesseract-explorer/src/reducers/index.js
@@ -8,13 +8,13 @@ import initialStateBuilder from "../utils/initialState";
 
 // this is the same as redux.combineReducers
 export default (state, action) => {
-  const explorerState = (state.explorer || state) ?? initialStateBuilder();
+  const explorerState = (state && state.hasOwnProperty("explorer") ? state.explorer : state) ?? initialStateBuilder();
   return {
     explorerAggregation: aggregationReducer(explorerState.explorerAggregation, action),
     explorerCubes: cubesReducer(explorerState.explorerCubes, action),
     explorerLoading: loadingReducer(explorerState.explorerLoading, action),
     explorerQuery: queryReducer(explorerState.explorerQuery, action),
     explorerStarred: starredReducer(explorerState.explorerStarred, action),
-    explorerUi: uiReducer(explorerState.explorerUi, action),
+    explorerUi: uiReducer(explorerState.explorerUi, action)
   };
 };
