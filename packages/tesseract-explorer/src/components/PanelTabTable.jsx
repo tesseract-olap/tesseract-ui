c14b2a1230c5de6abc23c81e6d5a7f90c01d4297
Francisco Abarzúa <git.id@frabarz.cl>
2020-04-15 20:04:38 -0400

    adds formatters for numeric columns

--- packages/tesseract-explorer/src/components/PanelTabTable.jsx
+++ packages/tesseract-explorer/src/components/PanelTabTable.jsx
@@ -1,9 +1,18 @@
-import {NonIdealState, Menu, MenuItem} from "@blueprintjs/core";
+import {NonIdealState, Menu, MenuItem, MenuDivider} from "@blueprintjs/core";
 import {Cell, Column, Table, ColumnHeaderCell} from "@blueprintjs/table";
 import classNames from "classnames";
 import memoizeOne from "memoize-one";
 import React, {PureComponent} from "react";
 import {sortByKey} from "../utils/array";
+import {formatAbbreviate} from "d3plus-format";
+
+const formatters = {
+  identity: n => `${n}`,
+  decimal: new Intl.NumberFormat(undefined, {useGrouping: false}).format,
+  human: n => formatAbbreviate(n, "en-US"),
+  million: new Intl.NumberFormat(undefined, {useGrouping: true}).format,
+  usd: new Intl.NumberFormat(undefined, {style: "currency", currency: "USD"}).format
+};
 
 /**
  * @typedef OwnProps
@@ -22,7 +31,8 @@ import {sortByKey} from "../utils/array";
 class TableTabPanel extends PureComponent {
   state = {
     sortKey: "",
-    sortDescending: true
+    sortDescending: true,
+    numberFormats: {}
   };
 
   cellClipboardData(data, keys, rowIndex, columnIndex) {
@@ -35,9 +45,15 @@ class TableTabPanel extends PureComponent {
     this.setState({sortKey, sortDescending});
   }
 
+  setNumberFormat(columnKey, format) {
+    this.setState(state => ({
+      numberFormats: {...state.numberFormats, [columnKey]: format}
+    }));
+  }
+
   render() {
     const {data} = this.props;
-    const {sortDescending, sortKey} = this.state;
+    const {numberFormats, sortDescending, sortKey} = this.state;
 
     if (data.length === 0) {
       return (
@@ -55,19 +71,20 @@ class TableTabPanel extends PureComponent {
     const columnKeys = Object.keys(sortedData[0]);
     const columnTypes = columnKeys.map(key => typeof sortedData[0][key]);
     const columns = columnKeys.map((columnKey, columnIndex) => {
-      const cellRenderer = rowIndex => (
+      const columnType = columnTypes[columnIndex];
+      const formatterName = numberFormats[columnKey] || (columnType === "number" ? "decimal" : "identity");
+      const formatter = formatters[formatterName];
+      const cellRenderer = rowIndex =>
         <Cell
-          className={`column-${columnTypes[columnIndex]}`}
+          className={`column-${columnType}`}
           columnIndex={columnIndex}
           rowIndex={rowIndex}
         >
-          {sortedData[rowIndex][columnKey]}
-        </Cell>
-      );
-      const menuRenderer = this.renderColumnMenu.bind(this, columnKey);
-      const columnHeaderCellRenderer = () => (
-        <ColumnHeaderCell name={columnKey} menuRenderer={menuRenderer} />
-      );
+          {formatter(sortedData[rowIndex][columnKey])}
+        </Cell>;
+      const menuRenderer = this.renderColumnMenu.bind(this, columnKey, columnType);
+      const columnHeaderCellRenderer = () =>
+        <ColumnHeaderCell name={columnKey} menuRenderer={menuRenderer} />;
       return (
         <Column
           cellRenderer={cellRenderer}
@@ -97,7 +114,24 @@ class TableTabPanel extends PureComponent {
     );
   }
 
-  renderColumnMenu(columnKey) {
+  /**
+   * @param {string} columnKey
+   * @param {"string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"} columnType
+   */
+  renderColumnMenu(columnKey, columnType) {
+    const formatName = this.state.numberFormats[columnKey] || "decimal";
+    const numericOptions = columnType === "number" &&
+      <React.Fragment>
+        <MenuDivider title="Numeral format" />
+        {["decimal", "million", "human", "usd"].map(format =>
+          <MenuItem
+            key={format}
+            icon={formatName === format ? "selection" : "circle"}
+            onClick={this.setNumberFormat.bind(this, columnKey, format)}
+            text={formatters[format](12345.678)}
+          />)}
+      </React.Fragment>;
+
     return (
       <Menu>
         <MenuItem
@@ -110,6 +144,7 @@ class TableTabPanel extends PureComponent {
           onClick={this.setSortingHandler.bind(this, columnKey, true)}
           text="Sort Desc"
         />
+        {numericOptions}
       </Menu>
     );
   }
