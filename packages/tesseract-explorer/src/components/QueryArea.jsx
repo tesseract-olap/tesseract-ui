ade6276230e771c6c4798a8716939888946ee4f7
Dave Landry <landry.dave@gmail.com>
2021-01-12 10:31:59 -0500

    adds collapsable toggle prop to QueryArea

--- packages/tesseract-explorer/src/components/QueryArea.jsx
+++ packages/tesseract-explorer/src/components/QueryArea.jsx
@@ -1,11 +1,12 @@
 import {Button, ButtonGroup, Divider, Icon, Intent} from "@blueprintjs/core";
-import React from "react";
+import React, {Fragment} from "react";
 import ScrollArea from "react-shadow-scroll";
 import classNames from "classnames";
 
 /**
  * @typedef OwnProps
  * @property {boolean} [open]
+ * @property {boolean} [collapsable]
  * @property {string} [className]
  * @property {string} title
  * @property {React.ReactNode} [toolbar]
@@ -14,10 +15,11 @@ import classNames from "classnames";
  */
 
 /** @type {React.FC<OwnProps>} */
-const QueryArea = ({children, className, open = true, stopBubbling, title, toolbar, warning}) =>
-  <details className={classNames("query-area", className)} open={open}>
+const QueryArea = ({children, className, collapsable = true, open = true, stopBubbling, title, toolbar, warning}) => {
+
+  const content = <Fragment>
     <summary className="details-title">
-      <Icon className="icon-chevron" icon="chevron-right" />
+      <Icon className="icon-chevron" icon={collapsable ? "chevron-right" : "blank"} />
       <span className="title">{title}</span>
       <ButtonGroup minimal onClick={stopBubbling}>
         {toolbar}
@@ -26,7 +28,12 @@ const QueryArea = ({children, className, open = true, stopBubbling, title, toolb
       </ButtonGroup>
     </summary>
     <ScrollArea isShadow={false}>{children}</ScrollArea>
-  </details>;
+  </Fragment>;
+
+  return collapsable
+    ? <details className={classNames("query-area", className)} open={open}>{content}</details>
+    : <div className={classNames("query-area", className)}>{content}</div>;
+};
 
 QueryArea.defaultProps = {
   stopBubbling(evt) {
