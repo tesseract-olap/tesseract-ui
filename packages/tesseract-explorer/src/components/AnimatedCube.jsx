87fb508d77964478c30f19b0cf1c37bede4c0eb8
Francisco Abarzua <git.id@frabarz.cl>
2021-05-03 16:19:32 -0400

    attempts to simplify some stylesheets changes CubeAnimation from css to svg

--- packages/tesseract-explorer/src/components/AnimatedCube.jsx
+++ packages/tesseract-explorer/src/components/AnimatedCube.jsx
@@ -8,15 +8,15 @@ import classNames from "classnames";
 
 /** @type {React.FC<OwnProps>} */
 export const AnimatedCube = props =>
-  <div className={classNames("animated-cube", props.className)}>
-    <div className="cube">
-      <span />
-      <span />
-      <span />
-    </div>
-    <div className="cube">
-      <span />
-      <span />
-      <span />
-    </div>
-  </div>;
+  <svg
+    className={classNames("animated-cube", props.className)}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+  >
+    <path className="a" d="M128 128v128l111 -64v-128l-111 64z" />
+    <path className="b" d="M128 128l111 -64l-111 -64l-111 64l111 64z" />
+    <path className="c" d="M128 128l-111 -64v128l111 64v-128z" />
+    <path className="a" d="M128 128v-64l-55 32v64l55 -32z"/>
+    <path className="b" d="M128 128l-55 32l55 32l55 -32l-55 -32z" />
+    <path className="c" d="M128 128l55 32v-64l-55 -32v64z"/>
+  </svg>;
