2b753f0df162568a85a54a1f9f87597e64d7591e
Francisco Abarzua <git.id@frabarz.cl>
2021-08-05 19:34:27 -0400

    fixes worker missing libs injected after transpile

--- packages/tesseract-explorer/src/utils/pivot.js
+++ packages/tesseract-explorer/src/utils/pivot.js
@@ -1,24 +1,46 @@
 /**
+ * @template T
+ * @typedef {(initial: string, set: T[]) => [string, ...T[]]} Concatter
+ */
+
+/**
  * Converts an array of tidy data into a pivotted table.
+ *
  * @param {Record<string, number>[]} data
  * @param {Record<"rowProp" | "colProp" | "valProp", string>} sides
+ *
+ * @param {object} cls
+ * This is to provide some raw structs needed in the function,
+ * without having them modified by the transpiler.
+ * @param {typeof Array} cls.Array
+ * @param {typeof Map} cls.Map
+ * @param {typeof Set} cls.Set
+ * @param {Concatter<number | undefined>} cls.cc
+ *
  * @returns {JSONArrays}
  */
-export function serializeTidyToArrays(data, sides) {
+export function serializeTidyToArrays(data, sides, {Array, Map, Set, cc}) {
   const {colProp, rowProp, valProp} = sides;
 
   const colMembers = new Set();
   const rowMembers = new Set();
-  const valReference = new Map([["", data.slice(0, 0)]]);
 
-  const getReference = (colLabel, rowLabel) => {
-    const key = `<${colLabel}>x<${rowLabel}>`;
+  /** @type {Map<string, Record<string, number>[]>} */
+  const valReference = new Map();
+
+  /**
+   * @param {string} colLabel
+   * @param {string} rowLabel
+   * @returns {Record<string, number>[]}
+   */
+  function getReference(colLabel, rowLabel) {
+    const key = "".concat("<", colLabel, ">x<", rowLabel, ">");
     const value = valReference.get(key);
     if (value !== undefined) return value;
     const defaultValue = [];
     valReference.set(key, defaultValue);
     return defaultValue;
-  };
+  }
 
   const n = data.length;
   let i = 0;
@@ -33,28 +55,34 @@ export function serializeTidyToArrays(data, sides) {
   }
 
   return {
-    headers: [rowProp, ...colMembers],
+    headers: cc(rowProp, Array.from(colMembers)),
     data: Array.from(rowMembers, rowLabel =>
-      [rowLabel, ...Array.from(colMembers, colLabel => {
-        const items = valReference.get(`<${colLabel}>x<${rowLabel}>`);
+      cc(rowLabel, Array.from(colMembers, colLabel => {
+        const key = "".concat("<", colLabel, ">x<", rowLabel, ">");
+        const items = valReference.get(key);
         if (!items || items.length === 0) return undefined;
         const value = items.reduce((sum, datum) => sum + datum[valProp], 0);
         return isNaN(value) ? undefined : value;
-      })]
+      }))
     )
   };
 }
 
 /**
- *
  * @param {Record<string, any>[]} data
  * @param {Record<"rowProp" | "colProp" | "valProp", string>} sides
  * @returns {Promise<JSONArrays>}
  */
 export function serializeToArray(data, sides) {
-  if (!Blob) {
+  // If the browser doesn't support Blobs or WebWorkers
+  if (!Blob || !Worker) {
     return new Promise(resolve => {
-      const result = serializeTidyToArrays(data, sides);
+      const result = serializeTidyToArrays(data, sides, {
+        Array,
+        Set,
+        Map,
+        cc: (initial, set) => [initial, ...set]
+      });
       resolve(result);
     });
   }
@@ -62,9 +90,12 @@ export function serializeToArray(data, sides) {
   const scriptBody = `
 self.onmessage = function(e) {
   const {data, sides} = e.data;
-  const result = (${serializeTidyToArrays})(data, sides);
+  const result = (${serializeTidyToArrays})(data, sides, {Array, Map, Set, cc});
   self.postMessage(result);
 };
+function cc(initial, set) {
+  return [initial, ...Array.from(set)];
+}
 `;
   const blob = new Blob([scriptBody], {type: "text/javascript"});
   return new Promise((resolve, reject) => {
@@ -97,15 +128,13 @@ export function stringifyMatrix(matrix, formatter, format) {
     const str = `${value}`.trim();
     return str.includes(joint) ? JSON.stringify(str) : str;
   };
-  const safeFormatter = value => value === undefined
-    ? ""
-    : safeQuoter(formatter(value));
+  const safeFormatter = value =>
+    value === undefined ? "" : safeQuoter(formatter(value));
 
   return [
     matrix.headers.map(safeQuoter).join(joint),
-    ...matrix.data.map(row => [
-      safeQuoter(row[0]),
-      ...row.slice(1).map(safeFormatter)
-    ].join(joint))
+    ...matrix.data.map(row =>
+      [safeQuoter(row[0]), ...row.slice(1).map(safeFormatter)].join(joint)
+    )
   ].join("\n");
 }
