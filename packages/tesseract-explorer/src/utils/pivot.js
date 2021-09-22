98b7187048829ddef2b11b2abe6edf42a1014fc3
Francisco Abarzua <git.id@frabarz.cl>
2021-09-22 16:38:54 -0300

    fixes column sorting on members for pivot view

--- packages/tesseract-explorer/src/utils/pivot.js
+++ packages/tesseract-explorer/src/utils/pivot.js
@@ -20,52 +20,72 @@
  * @returns {JSONArrays}
  */
 export function serializeTidyToArrays(data, sides, {Array, Map, Set, cc}) {
-  const {colProp, rowProp, valProp} = sides;
+  const {valProp} = sides;
+  const colProp = getIdProperty(sides.colProp);
+  const rowProp = getIdProperty(sides.rowProp);
 
   const colMembers = new Set();
   const rowMembers = new Set();
 
+  /** @type {Map<string, string>} */ const colDict = new Map();
+  /** @type {Map<string, string>} */ const rowDict = new Map();
+
   /** @type {Map<string, Record<string, number>[]>} */
   const valReference = new Map();
 
+  const n = data.length;
+  let i = 0;
+  while (i < n) {
+    const datum = data[i++];
+
+    const colId = datum[colProp];
+    const rowId = datum[rowProp];
+    colMembers.add(colId);
+    rowMembers.add(rowId);
+    getValueReference(colId, rowId).push(datum);
+
+    const colLabel = datum[sides.colProp];
+    const rowLabel = datum[sides.rowProp];
+    colDict.set(colId, colLabel);
+    rowDict.set(rowId, rowLabel);
+  }
+
+  const sortedCols = Array.from(colMembers).sort();
+
+  return {
+    headers: cc(rowProp, sortedCols.map(colId => colDict.get(colId) || colId)),
+    data: Array.from(rowMembers, rowId =>
+      cc(rowDict.get(rowId) || rowId, sortedCols.map(colId => {
+        const items = getValueReference(colId, rowId);
+        if (items.length === 0) return undefined;
+        const value = items.reduce((sum, datum) => sum + datum[valProp], 0);
+        return isNaN(value) ? undefined : value;
+      }))
+    )
+  };
+
   /**
-   * @param {string} colLabel
-   * @param {string} rowLabel
-   * @returns {Record<string, number>[]}
+   * Gets the property name for the ID of the required property.
+   * @type {(prop: string) => string}
    */
-  function getReference(colLabel, rowLabel) {
-    const key = "".concat("<", colLabel, ">x<", rowLabel, ">");
+  function getIdProperty(prop) {
+    return {}.hasOwnProperty.call(data[0], `${prop} ID`) ? `${prop} ID` : prop;
+  }
+
+  /**
+   * Retrieves the container array for the elements in the data that match a
+   * specified (col, row) tuple. If this array was not previously defined, is
+   * created.
+   * @type {(colId: string, rowId: string) => Record<string, number>[]}
+   */
+  function getValueReference(colId, rowId) {
+    const key = "".concat("<", colId, ">x<", rowId, ">");
     const value = valReference.get(key);
     if (value !== undefined) return value;
     const defaultValue = [];
     valReference.set(key, defaultValue);
     return defaultValue;
   }
-
-  const n = data.length;
-  let i = 0;
-  while (i < n) {
-    const datum = data[i++];
-    const colLabel = datum[colProp];
-    const rowLabel = datum[rowProp];
-
-    colMembers.add(colLabel);
-    rowMembers.add(rowLabel);
-    getReference(colLabel, rowLabel).push(datum);
-  }
-
-  return {
-    headers: cc(rowProp, Array.from(colMembers)),
-    data: Array.from(rowMembers, rowLabel =>
-      cc(rowLabel, Array.from(colMembers, colLabel => {
-        const key = "".concat("<", colLabel, ">x<", rowLabel, ">");
-        const items = valReference.get(key);
-        if (!items || items.length === 0) return undefined;
-        const value = items.reduce((sum, datum) => sum + datum[valProp], 0);
-        return isNaN(value) ? undefined : value;
-      }))
-    )
-  };
 }
 
 /**
