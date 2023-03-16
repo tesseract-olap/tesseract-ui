8e6275574471dd0e411e47bec1d5c92089e9d980
ffigueroal <felipe.figueroa.lara@gmail.com>
2023-03-16 17:39:19 -0300

    add non ideal state component

---
+++ packages/tesseract-explorer/src/components/NonIdealState.jsx
@@ -0,0 +1,22 @@
+import {Center, Stack, Text, Title} from "@mantine/core";
+import React from "react";
+
+/**
+ * @typedef OwnProps
+ * @property {React.ReactElement} [action]
+ * @property {React.ReactElement} [children]
+ * @property {string | React.ReactElement} [description]
+ * @property {React.ReactFragment | React.ReactElement<any, string | React.JSXElementConstructor<any>> | boolean} [icon]
+ * @property {string} [title]
+ */
+
+/** @type {React.FC<OwnProps>} */
+export const NonIdealState = props => <Center h="100%" w="100%">
+  <Stack align="center" spacing="xs">
+    {props.icon && props.icon}
+    {props.title && <Title order={5}>{props.title}</Title>}
+    {props.description && <Text>{props.description}</Text>}
+    {props.children && props.children}
+    {props.action && props.action} 
+  </Stack>
+</Center>;
