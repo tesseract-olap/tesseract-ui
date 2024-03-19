6752aa723346e19a5a2a636e515b1b74fbb86ab6
Francisco Abarzua <git.id@frabarz.cl>
2024-03-19 15:32:42 -0300

    updates nextjs example app

--- apps/nextjs13/next.config.js
+++ apps/nextjs13/next.config.js
@@ -2,9 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ['@datawheel/vizbuilder', 'd3plus-format'],
   env: {
     BUILD_VERSION: "x.y.z",
-    TESSERACT_SOURCE: process.env.OLAPPROXY_TARGET,
+    OLAPPROXY_JWTAUTH: process.env.OLAPPROXY_JWTAUTH,
+  },
+  async rewrites() {
+    return [{
+      source: "/olap",
+      destination: process.env.OLAPPROXY_TARGET
+    },{
+      source: "/olap/:path*",
+      destination: process.env.OLAPPROXY_TARGET + ":path*"
+    }]
   }
 };
 
--- apps/nextjs13/pages/index.jsx
+++ apps/nextjs13/pages/index.jsx
@@ -1,12 +1,26 @@
 import {DebugView, Explorer, PivotView, TableView} from "@datawheel/tesseract-explorer";
+import {createVizbuilderView} from "@datawheel/tesseract-vizbuilder";
 import Head from "next/head";
 import React from "react";
+
+import "normalize.css";
+
+const headers = {};
+if (process.env.OLAPPROXY_JWTAUTH) {
+  headers["x-tesseract-jwt-token"] = process.env.OLAPPROXY_JWTAUTH;
+}
+
+const VizbuilderPanel = createVizbuilderView({
+  chartTypes: ["barchart", "barchartyear", "lineplot", "stacked", "treemap", "geomap", "donut"],
+  downloadFormats: ["svg", "png"],
+  showConfidenceInt: false
+});
 
 const PANELS = [
   {key: "table", label: "Data Table", component: TableView},
   {key: "matrix", label: "Pivot Table", component: PivotView},
+  {key: "vizbuilder", label: "Vizbuilder", component: VizbuilderPanel},
   {key: "debug", label: "Raw response", component: DebugView}
-  // {key: "vizbuilder", label: "Vizbuilder", component: VizbuilderPanel}
 ];
 
 /** */
@@ -21,7 +35,10 @@ export default function Home() {
       <Explorer
         panels={PANELS}
         previewLimit={100}
-        source={process.env.OLAPPROXY_TARGET}
+        source={{
+          headers,
+          url: "/olap"
+        }}
         withinMantineProvider
         withinReduxProvider
         withMultiQuery
