c44b998bb5ae6f4eefe6dca97895abd0a494d60f
Francisco Abarzua <git.id@frabarz.cl>
2024-03-20 17:34:31 -0300

    updates customized tableview on example

--- apps/vitejs/src/TableView.jsx
+++ apps/vitejs/src/TableView.jsx
@@ -1,5 +1,5 @@
 import {TableView as BaseTableView} from "@datawheel/tesseract-explorer";
-import React, {useCallback} from "react";
+import React from "react";
 
 const columnOrder = [
   "Year",
@@ -19,34 +19,29 @@ const columnOrder = [
   "Public and Private Integrity"
 ];
 
+// Hide all columns labeled with "Something ID" except for "ISO ID"
+const columnFilter = item => item.label === "ISO ID" || !item.label.endsWith(" ID");
+
+// If a column label is in `columnOrder`, sort by its position, else put at the end
+const columnSorting = (a, b) => {
+  const indexA = columnOrder.indexOf(a.label);
+  const indexB = columnOrder.indexOf(b.label);
+  if (indexA === -1 && indexB === -1) {
+    return "".localeCompare.call(a.localeLabel, b.localeLabel);
+  }
+  if (indexA === -1) return 1;
+  if (indexB === -1) return -1;
+  return indexA - indexB;
+};
+
 /**
  * @param {import("@datawheel/tesseract-explorer").ViewProps} props
  */
-export const TableView = props => {
-  // Hide all columns labeled with "Something ID" except for "ISO ID"
-  const columnFilter = useCallback(item =>
-    item.label === "ISO ID" || !item.label.endsWith(" ID")
-  , []);
-
-  // If a column label is in `columnOrder`, sort by its position, else put at the end
-  const columnSorting = useCallback((a, b) => {
-    const indexA = columnOrder.indexOf(a.label);
-    const indexB = columnOrder.indexOf(b.label);
-    if (indexA === -1 && indexB === -1) {
-      return "".localeCompare.call(a.localeLabel, b.localeLabel);
-    }
-    if (indexA === -1) return 1;
-    if (indexB === -1) return -1;
-    return indexA - indexB;
-  }, []);
-
-  return (
-    <BaseTableView
-      cube={props.cube}
-      params={props.params}
-      result={props.result}
-      columnFilter={columnFilter}
-      columnSorting={columnSorting}
-    />
-  );
-};
+export const TableView = props =>
+  <BaseTableView
+    cube={props.cube}
+    params={props.params}
+    result={props.result}
+    columnFilter={columnFilter}
+    columnSorting={columnSorting}
+  />;
