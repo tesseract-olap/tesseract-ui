d298dc1491e4fec3755849ebe5a9764b261d9405
Francisco Abarzua <git.id@frabarz.cl>
2026-06-23 13:14:20 -0400

    upgrades vite dependency on demo app

--- apps/vitejs/vite.config.js
+++ apps/vitejs/vite.config.js
@@ -1,37 +1,36 @@
 import pluginReact from "@vitejs/plugin-react";
-import pluginLegacy from '@vitejs/plugin-legacy'
+import pluginLegacy from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 
 const {
   OLAPPROXY_BASICAUTH,
   OLAPPROXY_JWTAUTH,
-  OLAPPROXY_TARGET = "http://localhost:7777"
+  OLAPPROXY_TARGET = "http://localhost:7777",
 } = process.env;
 
-const target = new URL(OLAPPROXY_TARGET);
-target.pathname = `${target.pathname}/`.replace(/\/{2,}/g, "/");
+const proxyTarget = new URL(OLAPPROXY_TARGET);
 
 const headers = {};
 if (OLAPPROXY_JWTAUTH) {
   headers["x-tesseract-jwt-token"] = OLAPPROXY_JWTAUTH;
 }
 
-export default defineConfig(({command, mode, ssrBuild}) => {
-  const plugins = [pluginReact({fastRefresh: false})];
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const plugins = [pluginReact({ fastRefresh: false })];
 
   if (command == "build") {
     plugins.push(
       pluginLegacy({
         targets: ["defaults", "not IE 11"],
-      })
+      }),
     );
   }
 
   return {
     root: "./src",
     define: {
-      'process.env.BUILD_VERSION': '"x.y.z"',
-      'process.env': {}
+      "process.env.BUILD_VERSION": '"x.y.z"',
+      "process.env": {},
     },
     plugins,
     server: {
@@ -40,16 +39,16 @@ export default defineConfig(({command, mode, ssrBuild}) => {
           auth: OLAPPROXY_BASICAUTH,
           changeOrigin: true,
           secure: false,
-          target: target.origin,
+          target: proxyTarget.origin,
           followRedirects: true,
           headers,
           rewrite: (path) => {
-            const newPath = path.replace(/^\/olap\//, target.pathname);
-            console.log(target.origin, newPath);
+            const newPath = path.replace(/^\/olap\//, proxyTarget.pathname).replace(/\/{2,}/g, "/");
+            console.log(proxyTarget.origin, newPath);
             return newPath;
-          }
-        }
-      }
-    }
+          },
+        },
+      },
+    },
   };
-})
+});
